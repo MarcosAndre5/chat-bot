a4c8b7c7ec36709aeb07adf5a8d6303474c6efbd
Marcos André <marcosandreazevedo78@gmail.com>
2022-09-05 16:51:07 -0300

    Inicio implementação de arquivos de chamados.

--- enviarMensagem.php
+++ enviarMensagem.php
@@ -16,9 +16,9 @@ $twilio = new Client($sid, $token);
 
 $codes = ["chocolate", "baunilha", "morango", "mint-chocolate-chip", "cookies-n-cream"];
 
-$message = $twilio->messages->create("whatsapp:" . getenv("MY_WHATSAPP_NUMBER"), [
-		"body" => "Seu código de sorvete é " . $codes[rand(0,4)],
-		"from" => "whatsapp:" . getenv("TWILIO_WHATSAPP_NUMBER")
+$message = $twilio->messages->create("whatsapp:".getenv("MY_WHATSAPP_NUMBER2"), [
+		"body" => "Seu código de sorvete é ".$codes[rand(0,4)],
+		"from" => "whatsapp:".getenv("TWILIO_WHATSAPP_NUMBER")
 	]
 );
 
